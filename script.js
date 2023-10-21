669ad123bcdca2c293b50f87a570d355aa98fa71
ShreyasM8055 <115002248+ShreyasM8055@users.noreply.github.com>
2023-10-21 11:04:58 +0530

    Added all the files

---
+++ script.js
@@ -0,0 +1,27 @@
+// Handle login form submission
+const loginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+
+loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    alert(`Username: ${username}\nPassword: ${password}`);
+
+    // You can replace the alert with your actual login logic.
+});
+
+// Initialize Google Sign-In
+gapi.load('auth2', function() {
+    gapi.auth2.init({
+        client_id: '1065806393030-5l9rb4o7n2q8iobr13qav76em98rkl75.apps.googleusercontent.com'
+    }).then(function(auth2) {
+        // You can attach the click handler for your Google Sign-In button here
+        var options = new gapi.auth2.SigninOptionsBuilder();
+        options.setLoginHint('user@example.com'); // Replace with the user's email
+        auth2.attachClickHandler('google-signin', options, onSignIn, onFailure);
+    });
+});
